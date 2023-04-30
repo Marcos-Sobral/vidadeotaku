4a2c995fd7bd38687937c6c889f0c4278b6a1902
Marcos-Sobral <vini15oficial@gmail.com>
2023-04-29 23:16:32 -0300

    S1: :sparkles: feat:  pageinicial finalizada

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,11 +8,75 @@
 </head>
 <body>
     <livewire:header />
-        <main class="main">
+    <main class="main">
+        <div class="container grid-video">
             <div>
-                <h1 class="text-center">Lista de Animes adicionados</h1>
+                <h1 class="text-4xl font-bold">Como funciona o vida de otaku ?</h1>
+                <p class="text-medium mt-6 text-justify">
+                    Vida de Otaku é um sistema de gerenciamento de lista de animes que ajuda os usuários
+                    a organizar suas séries favoritas. Com recursos intuitivos e avançados, os usuários
+                    podem adicionar novos animes, marcar as séries já assistidas e até mesmo criar listas
+                    personalizadas. Com a Vida de Otaku, os amantes de anime têm uma ferramenta essencial
+                    para manter suas listas organizadas e atualizadas.
+                </p>
             </div>
-        </main>
+            <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/vZYMZKvMpm8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen style="max-width: 100%;"></iframe>
+            </div>
+        </div>
+        <div class="container ">
+            <div class="grid-container-info-main mt-20">
+                <div>
+                    <figure class="">
+                        <img src="{{env('APP_URL')}}/assets/img/item1.jpg" class="img-ilustration">
+                    </figure>
+                    <div class="px-4">
+                        <h1 class="text-lg mt-6 font-bold">1. Adicione os animes</h1>
+                        <p class="text-medium mt-6 text-justify">É possível fazer isso online de maneira simples. Basta selecionar, 
+                        importar ou adicionar os animes de seu interesse para uma lista futura.
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <figure>
+                        <img src="{{env('APP_URL')}}/assets/img/item2.jpg" class="img-ilustration">
+                    </figure>
+                    <div class="px-4">
+                        <h1 class="text-lg mt-6 font-bold">2. Minha primeira Lista</h1>
+                        <p class="text-medium mt-6 text-justify">Para criar uma lista, vá em “minha lista” , apenas selecione os que deseja e 
+                        atribuir um nome para Lista.
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <figure>
+                        <img src="{{env('APP_URL')}}/assets/img/item3.jpg" class="img-ilustration">
+                    </figure>
+                    <div class="px-4">
+                        <h1 class="text-lg mt-6 font-bold">3. Inove</h1>
+                        <p class="text-medium mt-6 text-justify">Dica para organizar suas lista, faça os 
+                            seguintes agrupamento: Assistindo, Pretendo Ver, Droppado, Pausado, Favoritos.
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <figure class="">
+                        <img src="{{env('APP_URL')}}/assets/img/item4.jpg" class="img-ilustration">
+                    </figure>
+                    <div class="px-4">
+                        <h1 class="text-lg mt-6 font-bold">4. Exporte suas listass</h1>
+                        <p class="text-medium mt-6 text-justify">
+                            Atráves do vida de otaku, é possível exportar suas listas, 
+                            caso queira migrar de plataforma 
+                        </p>
+                    </div>     
+                </div> 
+            </div>
+            <div class="p-10 text-center">
+                <x-Button>VAMOS COMEÇAR</x-Button>
+            </div> 
+        </div>
+    </main>
     <livewire:footer />
 </body>
 </html>
--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -5,61 +5,78 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-        #rodape-main{
+       footer {
+            background-color: #111827;
+            color: #fff;
             display: flex;
             justify-content: space-between;
-            background-color: #F3F3F3;
-            padding: 2% 5%;
-            flex-wrap: wrap;
+            padding: 5%;
         }
-        #rodape-main-ul{
+
+        ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+            display: grid;
+            grid-template-columns:repeat(3, 1fr);
+            text-align: center;
+        }
+
+        li {
+            display: inline-block;
+            margin-right: 20px;
+        }
+
+        a {
+            color: #fff;
             text-decoration: none;
-            list-style: none;
-            color: #161616;
         }
 
-        #rodape-main-link{
-            text-decoration: none;
-            list-style: none;
-            color: #161616;
-            line-height: 26px;
-            font-weight: bold;
+        .social a {
+            color: #fff;
+            margin-right: 20px;
         }
 
-        #rodape-info{
-            display: flex;
-            justify-content: space-between;
-            padding: 2% 5%;
+        .fa-facebook:before {
+            content: "\f09a";
         }
 
-        #rodape-info-linkIcon{
-            list-style: none;
-            color: #FBAB16;
-            font-weight: bold;
+        .fa-twitter:before {
+            content: "\f099";
         }
 
-        #btn{
-            width: 100%;
-            background-color: #FBAB16;
-            color: #F9F9F9;
-            padding: 4%;
-            border-radius: 10px;
-            margin-top: 20px;
-
+        .fa-instagram:before {
+            content: "\f16d";
         }
 
-        #input
-        {
-            padding: 12px 16px 14px;
-            width: 100%;
-            height: 50px;  
-            border: 2px solid #F9F9F9;
-            border-radius: 10px;
-            margin-top: 10px;
+        .fab {
+            font-family: "Font Awesome 5 Brands";
+            font-size: 18px;
+        }
+
+        @media (max-width: 800px) {
+            ul {
+                grid-template-columns:repeat(1, 1fr);
+            }
         }
     </style>
 </head>
 <body>
-
+    <footer>
+    <ul>
+        <li><a href="#">Dúvidas</a>
+            <p class="text-medium mt-6 text-justify">(63) xxxxx-xxxx</p>
+        </li>
+        <li><a href="#">Sobre nós</a>
+        <p class="text-medium mt-6 text-justify">O vida de otaku é um sistema voltado para o 
+            público que Brasileiro que ama a cultura de animações japonesas tanto quanto nós.</p>
+        </li>
+        <li  class="social">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+        </li>
+    </ul>
+    </footer>
 </body>
 </html>
