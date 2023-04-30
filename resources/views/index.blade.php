2fc5f0cc6038480d32bd4f40ca11354091390749
Marcos-Sobral <vini15oficial@gmail.com>
2023-04-29 23:57:24 -0300

    S1: :zap: perf: Link das fotos page about

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,7 +28,7 @@
             <div class="grid-container-info-main mt-20">
                 <div>
                     <figure class="">
-                        <img src="{{env('APP_URL')}}/assets/img/item1.jpg" class="img-ilustration">
+                        <img src="https://img.freepik.com/vector-gratis/ilustracion-otaku-dibujada-mano_23-2149662516.jpg?w=740&t=st=1682821566~exp=1682822166~hmac=b808801936dd5e05cca195f73e93e00fba84d40256df945f1503c52a6c11fd6b" class="img-ilustration">
                     </figure>
                     <div class="px-4">
                         <h1 class="text-lg mt-6 font-bold">1. Adicione os animes</h1>
@@ -39,7 +39,7 @@
                 </div>
                 <div>
                     <figure>
-                        <img src="{{env('APP_URL')}}/assets/img/item2.jpg" class="img-ilustration">
+                        <img src="https://img.freepik.com/vetores-gratis/ilustracao-do-conceito-do-aplicativo-do-curso_114360-6649.jpg?w=740&t=st=1682823209~exp=1682823809~hmac=ce5d12217cc0f396df07d46e8edeac1a6fcf36abbb74bea823b5cb92fce887f9" class="img-ilustration">
                     </figure>
                     <div class="px-4">
                         <h1 class="text-lg mt-6 font-bold">2. Minha primeira Lista</h1>
@@ -50,7 +50,7 @@
                 </div>
                 <div>
                     <figure>
-                        <img src="{{env('APP_URL')}}/assets/img/item3.jpg" class="img-ilustration">
+                        <img src="https://img.freepik.com/free-vector/video-tutorial-watching-online-lecture-internet-course-digital-lesson-tutor-cartoon-character-video-call-seminar-remote-education_335657-3439.jpg?w=740&t=st=1682822460~exp=1682823060~hmac=47d4b1e5d3796866ba7064e9f4c59b49e8232d9367c8430c3a2863d31cbab48f" class="img-ilustration">
                     </figure>
                     <div class="px-4">
                         <h1 class="text-lg mt-6 font-bold">3. Inove</h1>
@@ -61,7 +61,7 @@
                 </div>
                 <div>
                     <figure class="">
-                        <img src="{{env('APP_URL')}}/assets/img/item4.jpg" class="img-ilustration">
+                        <img src="https://img.freepik.com/free-vector/photo-sharing-concept-illustration_114360-425.jpg?w=740&t=st=1682822659~exp=1682823259~hmac=02313a25185eb2f1e2532001975cecdb73aa7ceecc0a3bf01b4f04e63ee8dc4f" class="img-ilustration">
                     </figure>
                     <div class="px-4">
                         <h1 class="text-lg mt-6 font-bold">4. Exporte suas listass</h1>
