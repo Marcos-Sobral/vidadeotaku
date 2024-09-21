1246e2390db1e0e09211bf2024114bdc48cba4ea
Marcos-Sobral <vini15oficial@gmail.com>
2024-09-21 20:48:38 -0300

    ♻️: S0: refactor: Model: one-to-many Item e sub itens

---
+++ app/Models/Sub_itens.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sub_itens extends Model
+{
+    use HasFactory;
+
+    protected $table = "sub_itens";
+    protected $primaryKey = "id_sub_item";
+
+    protected $fillable = [
+        "titulo_sub_item",
+        "arquivo_sub_item",
+        "link_sub_item"
+    ];
+
+    public function Item(){
+        return $this->belongsTo(Item::class, 'sub_item_id_item','id_item');
+        //O sub item tem um item
+    }
+}
