29ddc6f028ae2f42e5e9b9fd91e0785d3543cdf1
Marcos-Sobral <vini15oficial@gmail.com>
2024-09-21 20:42:45 -0300

    ♻️: S0: refactor: Model: many-to-many Item e genero

---
+++ app/Models/Genero_item.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Genero_item extends Model
+{
+    use HasFactory;
+
+    protected $table = "genero_itens";
+    protected $primaryKey = "id_genero_item";
+
+    protected $fillable = [
+        "nome_genero_item",
+
+    ];
+
+    public function Item(){
+        return $this->belongsToMany(Item::class, 'item_genero_id_item','id_item');
+        //O item tem varios generos
+    }
+}
