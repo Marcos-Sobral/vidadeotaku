6766dd36fb398f97d72f348677176a879d4038c8
Marcos-Sobral <vini15oficial@gmail.com>
2024-09-22 03:45:22 -0300

    ✨: S3: feat: instalando o bootstrap

--- resources/views/Pages/Listas/index.blade.php
+++ resources/views/Pages/Listas/index.blade.php
@@ -1,12 +1,13 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     
+                <h1 class="fs-2 text-center font-bold mb-4">Listas</h1>
+                    <a href="{{ route('admin.lista.create') }}" class="btn btn-primary mb-4">Criar Listas</a>
 
 
-                
                 </div>
             </div>
         </div>
