83277cb2d5f961acd9ee3e1745521309fcfa190e
Marcos-Sobral <vini15oficial@gmail.com>
2024-09-22 13:16:21 -0300

    ♻️: S3: refactor: admin: navigation correcao na posicao do siderbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,8 @@
             <div class="flex">
 
                 <!-- Botão para ocultar/exibir a sidebar -->
-                <div class="">
-                    <button @click="sidebarOpen = !sidebarOpen" class="p-2 bg-gray-200 text-gray-700 rounded-md hover:bg-gray-300 focus:outline-none mt-4 ml-4 inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+                <div class="d-flex">
+                    <button @click="sidebarOpen = !sidebarOpen" class="p-2 bg-gray-200 text-gray-700 rounded-md hover:bg-gray-300 focus:outline-none inline-flex items-center justify-center rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                         <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                             <path :class="{'hidden': sidebarOpen, 'inline-flex': ! sidebarOpen }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                             <path :class="{'hidden': ! sidebarOpen, 'inline-flex': sidebarOpen }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
