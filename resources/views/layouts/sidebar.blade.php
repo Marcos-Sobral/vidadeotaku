53dfa2da1ee1b67e2d33a7d38ab91772ea2e4731
Marcos-Sobral <vini15oficial@gmail.com>
2024-09-22 02:35:38 -0300

    ♻️: S3: refactor: siderbar: estilizada

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,14 +1,6 @@
 <div class="bg-gray-800 text-white w-full min-h-screen">
     <!-- Título da página atual -->
-    <div class="p-4">
-
-        <div class="pt-2 pb-3 space-y-1">
-            <h1 class="text-sm font-medium">Página atual:</h1>
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-
+    <div class="p-6">
         <!-- Informações do usuário -->
         <div class="flex items-center mt-4">
         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
@@ -18,38 +10,128 @@
                 <p class="text-xs text-gray-400">Administrador</p>
             </div>
         </div>
-
-        <!-- Botão de sair da conta -->
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <button type="submit" class="block w-full text-left py-2.5 px-4 mt-4 rounded hover:bg-gray-700">
-                Sair da Conta
-            </button>
-        </form>
     </div>
+
+    <hr class="mb-6">
 
     <!-- Menus -->
     <nav class="mt-5">
-        <a href="{{ route('dashboard') }}" class="block py-2.5 px-4 rounded hover:bg-gray-700">
-            Home
+        <a href="{{ route('dashboard') }}" class="flex align-items-center py-2 px-4 rounded hover:bg-gray-700 mt-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" >
+            <img src="{{ URL::asset('assets/img/icon/icons8-user-folder-50.png') }}" class="mr-2  w-6 h-6">
+            Carrossel
         </a>
 
-        <!-- Menu com Submenu -->
+        <!-- Gestão de pessoas-->
         <div x-data="{ open: false }" class="mt-2">
-            <button @click="open = ! open" class="w-full flex justify-between items-center py-2.5 px-4 hover:bg-gray-700 rounded">
-                <span>Gerenciamento</span>
-                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+            <button @click="open = ! open" class="w-full flex justify-between items-center block py-2 px-4 rounded hover:bg-gray-700 mt-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <img src="{{ URL::asset('assets/img/icon/icons8-users-30.png') }}" class="mr-2 w-6 h-6">
+                <span>Gestão de pessoas</span>
+                <svg stroke="currentColor" fill="none" viewBox="0 0 24 24" class="w-4 h-4">
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z" />
                 </svg>
             </button>
             <div x-show="open" class="mt-2 space-y-2 pl-4">
-                <a href="#" class="block py-2 px-4 hover:bg-gray-700 rounded">Usuários</a>
-                <a href="#" class="block py-2 px-4 hover:bg-gray-700 rounded">Projetos</a>
-                <a href="#" class="block py-2 px-4 hover:bg-gray-700 rounded">Relatórios</a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">            
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-folder-50.png') }}" class="mr-2 w-6 h-6">
+                    Usuários
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-shield-50.png') }}" class="mr-2 w-6 h-6">
+                    Nível de acesso
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">
+                    <img src="{{ URL::asset('assets/img/icon/icons8-analyzing-skill-50.png') }}" class="mr-2 w-6 h-6">
+                    Relatório de usuários
+                </a>
             </div>
         </div>
 
-        <a href="#" class="block py-2.5 px-4 rounded hover:bg-gray-700 mt-2">
+        <!-- Gestão de conteudo -->
+        <div x-data="{ open: false }" class="mt-2">
+            <button @click="open = ! open" class="w-full flex justify-between items-center block py-2 px-4 rounded hover:bg-gray-700 mt-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <img src="{{ URL::asset('assets/img/icon/icons8-file-64.png') }}" class="mr-2 w-6 h-6">
+                <span>Gestão dos itens</span>
+                <svg stroke="currentColor" fill="none" viewBox="0 0 24 24" class="w-4 h-4">
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z" />
+                </svg>
+            </button>
+            <div x-show="open" class="mt-2 space-y-2 pl-4">
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">            
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-folder-50.png') }}" class="mr-2 w-6 h-6">
+                    Conteúdo
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">            
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-folder-50.png') }}" class="mr-2 w-6 h-6">
+                    Sub Conteúdo
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-shield-50.png') }}" class="mr-2 w-6 h-6">
+                    Gênero
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-shield-50.png') }}" class="mr-2 w-6 h-6">
+                    Autor
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-shield-50.png') }}" class="mr-2 w-6 h-6">
+                    Status
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-shield-50.png') }}" class="mr-2 w-6 h-6">
+                    Tipo
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">
+                    <img src="{{ URL::asset('assets/img/icon/icons8-analyzing-skill-50.png') }}" class="mr-2 w-6 h-6">
+                    Relatório de conteúdo
+                </a>
+            </div>
+        </div>
+
+        <!-- gestão de lista -->
+        <div x-data="{ open: false }" class="mt-2">
+            <button @click="open = ! open" class="w-full flex justify-between items-center block py-2 px-4 rounded hover:bg-gray-700 mt-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <img src="{{ URL::asset('assets/img/icon/icons8-list-50.png') }}" class="mr-2 w-6 h-6">
+                <span>Gestão das Listas</span>
+                <svg stroke="currentColor" fill="none" viewBox="0 0 24 24" class="w-4 h-4">
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z" />
+                </svg>
+            </button>
+            <div x-show="open" class="mt-2 space-y-2 pl-4">
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">            
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-folder-50.png') }}" class="mr-2 w-6 h-6">
+                    Conteúdo
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">            
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-folder-50.png') }}" class="mr-2 w-6 h-6">
+                    Sub Conteúdo
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-shield-50.png') }}" class="mr-2 w-6 h-6">
+                    Gênero
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-shield-50.png') }}" class="mr-2 w-6 h-6">
+                    Autor
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-shield-50.png') }}" class="mr-2 w-6 h-6">
+                    Status
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">
+                    <img src="{{ URL::asset('assets/img/icon/icons8-user-shield-50.png') }}" class="mr-2 w-6 h-6">
+                    Tipo
+                </a>
+                <a href="#" class="flex py-2 px-6 hover:bg-gray-700 rounded">
+                    <img src="{{ URL::asset('assets/img/icon/icons8-analyzing-skill-50.png') }}" class="mr-2 w-6 h-6">
+                    Relatório de conteúdo
+                </a>
+            </div>
+        </div>
+
+        <a href="#" class="block py-2 px-4 rounded hover:bg-gray-700 mt-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
             Configurações
         </a>
     </nav>
